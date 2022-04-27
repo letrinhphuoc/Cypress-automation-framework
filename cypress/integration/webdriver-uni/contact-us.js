9c9a65bc521886f8bb5433c29a36840c7d728e45
Phuoc Le <phuoc.le@vn.woowahan.com>
2022-04-28 00:20:52 +0700

    cypress with cucumber

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -26,10 +26,14 @@ describe("Test Contact Us form via WebdriverUni", () => {
     cy.document().should("have.property", "charset").and("eq", "UTF-8");
     cy.title().should("include", "WebDriver | Contact Us");
     cy.url().should("include", "contactus");
-    // contact_Us_PO.contactForm_Submission(Cypress.env("first_name"), data.last_name, data.email, "How can I learn Cypress?", "h1", "Thank You for your Message!failed");
+    contact_Us_PO.contactForm_Submission(Cypress.env("first_name"), data.last_name, data.email, "How can I learn Cypress?", "h1", "Thank You for your Message!");
   });
 
   it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-    contact_Us_PO.contactForm_Submission(data.first_name, data.last_name," ", "How can I learn Cypress?", "body", "Error: Invalid email address")
+    if(Cypress.isBrowser('firefox')) {
+
+    } else {
+      contact_Us_PO.contactForm_Submission(data.first_name, data.last_name," ", "How can I learn Cypress?", "body", "Error: Invalid email address")
+    }
   });
 });
